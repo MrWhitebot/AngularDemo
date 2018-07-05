2ee3ce8dbb0b2f586607c0d2a05aded524070f70
Blanco Manuel <59001436@esfds.net>
2018-07-05 14:06:07 +0200

    Servicio creado, header y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpModule } from "@angular/http";
+
 // Rutas
-import {app_routing} from './app.rutas';
+import {app_routing} from "./app.rutas";
+
+// Servicios
+import {InformacionService} from "./services/informacion.service";
 
 // Componentes
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { PortafolioComponent } from './components/portafolio/portafolio.component';
-import { AboutComponent } from './components/about/about.component';
-import { ItemComponent } from './components/item/item.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { PortafolioComponent } from "./components/portafolio/portafolio.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ItemComponent } from "./components/item/item.component";
 
 @NgModule({
   declarations: [
@@ -22,9 +27,13 @@ import { ItemComponent } from './components/item/item.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     app_routing
+
   ],
-  providers: [],
+  providers: [
+    InformacionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.rutas.ts
+++ src/app/app.rutas.ts
@@ -1,11 +1,11 @@
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from "@angular/router";
 
-import {AboutComponent, ItemComponent, PortafolioComponent} from './components/index.paginas';
+import {AboutComponent, ItemComponent, PortafolioComponent} from "./components/index.paginas";
 
 const app_routes: Routes = [
-  {path: 'home', component: PortafolioComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'item', component: ItemComponent},
-  {path: '**', pathMatch: 'full', redirectTo: ''}
+  {path: "home", component: PortafolioComponent},
+  {path: "about", component: AboutComponent},
+  {path: "item", component: ItemComponent},
+  {path: "**", pathMatch: "full", redirectTo: ""}
 ];
 export const app_routing = RouterModule.forRoot(app_routes);
